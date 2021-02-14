90b484e108a993545fada01e2fe8dfcc7bccd66b
Rio Harapan Ps <rioharapan@gmail.com>
2021-02-14 16:20:51 +0700

    Lesson 6 Writing better routes and serving external files - part static file with routing

--- part01/serve_html/main.js
+++ part01/serve_html/main.js
@@ -9,18 +9,23 @@ const routeMap = {
     "/" : "views/index.html"
 };
 
-http.createServer((req, res) => {
-    res.writeHead(httpStatus.OK, {
-        "Content-Type" : "text/html"
-    });
+const getViewUrl = (url) => {
+    return `views/${url}.html`;
+}
 
-    if (routeMap[req.url]) {
-        fs.readFile(routeMap[req.url], (error, data) => {
+http.createServer((req, res) => {
+    let viewUrl = getViewUrl(req.url);
+    fs.readFile(viewUrl, (error, data) => {
+        if (error) {
+            res.writeHead(httpStatus.NOT_FOUND);
+            res.write("<h1>FILE NOT FOUND</h1>");
+        } else {
+            res.writeHead(httpStatus.OK, {
+                "Content-Type" : "text/html"
+            });
             res.write(data);
-            res.end();
-        });
-    } else {
-        res.end("<h1>Sorry, not found.</h1>");
-    }
+        }
+        res.end();
+    });
 }).listen(port);
 console.log(`The server has started and is listening on port number: ${port}`);
