8a3ffbbe508a339e0617a72bbe913aa472e8f6f7
Rio Harapan Ps <rioharapan@gmail.com>
2021-02-16 00:10:30 +0700

    Lesson 6 Writing better routes and serving external files - routing complete

--- part01/serve_html/main.js
+++ part01/serve_html/main.js
@@ -1,55 +1,37 @@
-const { error } = require("console");
-
-const port = 3000;
-http = require("http");
-httpStatus = require("http-status-codes");
-fs = require("fs");
-
-const sendErrorResponse = res => {
-    res.writeHead(httpStatus.NOT_FOUND, {
+const port = 3000,
+    http = require("http"),
+    httpStatusCodes = require("http-status-codes"),
+    fs = require("fs"),
+    router = require("./route"),
+    plainTextContentType = {
+        "Content-Type": "text/plain"
+    },
+    htmlContenType = {
         "Content-Type": "text/html"
-    });
-    res.write("<h1>File Not Found!</h1>");
-    res.end();
-};
-
-const customReadFile = (file_path, res) => {
-    if (fs.existsSync(file_path)) {
-        fs.readFile(file_path, (error, data) => {
-            if (error) {
-                console.log(error);
-                sendErrorResponse(res);
-                return;
+    },
+    customReadFile = (file, res) => {
+        fs.readFile(`./${file}`, (errors, data) => {
+            if (errors) {
+                console.log("Error reading the file...");
             }
-            res.write(data);
-            res.end();
+            res.end(data);
         });
-    } else {
-        sendErrorResponse(res);
-    }
-    console.log(`Accessed : ${file_path}`)
-};
+    };
 
-http.createServer((req, res) => {
-    let url = req.url;
-    if (url.indexOf(".html") != -1) {
-        res.writeHead(httpStatus.OK, {
-            "Content-Type": "text/html"
-        });
-        customReadFile(`./views${url}`, res);
-    } else if (url.indexOf(".css") !== -1) {
-        res.writeHead(httpStatus.OK, {
-            "Content-Type" : "text/css"
-        });
-        customReadFile(`./public/css${url}`, res);
-    } else if (url.indexOf(".png") !== -1) {
-        res.writeHead(httpStatus.OK, {
-            "Content-Type" : "image/png"
-        });
-        customReadFile(`./public/images${url}`, res);
-    } else {
-        sendErrorResponse(res);
-    }
-}).listen(3000);
+router.get("/", (req, res) => {
+    res.writeHead(httpStatusCodes.OK, plainTextContentType);
+    res.end("INDEX");
+});
 
-console.log(`The server is listening on port number: ${port}`);
+router.get("/index.html", (req, res) => {
+    res.writeHead(httpStatusCodes.OK, htmlContenType);
+    customReadFile("/views/index.html", res);
+});
+
+router.post("/", (req, res) => {
+    res.writeHead(httpStatusCodes.OK, plainTextContentType);
+    res.end("POSTED");
+});
+
+http.createServer(router.handle).listen(3000);
+console.log(`The server is listening on port number ${port}`);
