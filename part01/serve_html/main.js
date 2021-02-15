0a37f5c9ed5c8a50c38af384b01b96f3b33c2459
Rio Harapan Ps <rioharapan@gmail.com>
2021-02-15 15:59:16 +0700

    Lesson 6 Writing better routes and serving external files - part routing based file

--- part01/serve_html/main.js
+++ part01/serve_html/main.js
@@ -5,27 +5,51 @@ http = require("http");
 httpStatus = require("http-status-codes");
 fs = require("fs");
 
-const routeMap = {
-    "/" : "views/index.html"
+const sendErrorResponse = res => {
+    res.writeHead(httpStatus.NOT_FOUND, {
+        "Content-Type": "text/html"
+    });
+    res.write("<h1>File Not Found!</h1>");
+    res.end();
 };
 
-const getViewUrl = (url) => {
-    return `views/${url}.html`;
-}
+const customReadFile = (file_path, res) => {
+    if (fs.existsSync(file_path)) {
+        fs.readFile(file_path, (error, data) => {
+            if (error) {
+                console.log(error);
+                sendErrorResponse(res);
+                return;
+            }
+            res.write(data);
+            res.end();
+        });
+    } else {
+        sendErrorResponse(res);
+    }
+    console.log(`Accessed : ${file_path}`)
+};
 
 http.createServer((req, res) => {
-    let viewUrl = getViewUrl(req.url);
-    fs.readFile(viewUrl, (error, data) => {
-        if (error) {
-            res.writeHead(httpStatus.NOT_FOUND);
-            res.write("<h1>FILE NOT FOUND</h1>");
-        } else {
-            res.writeHead(httpStatus.OK, {
-                "Content-Type" : "text/html"
-            });
-            res.write(data);
-        }
-        res.end();
-    });
-}).listen(port);
-console.log(`The server has started and is listening on port number: ${port}`);
+    let url = req.url;
+    if (url.indexOf(".html") != -1) {
+        res.writeHead(httpStatus.OK, {
+            "Content-Type": "text/html"
+        });
+        customReadFile(`./views${url}`, res);
+    } else if (url.indexOf(".css") !== -1) {
+        res.writeHead(httpStatus.OK, {
+            "Content-Type" : "text/css"
+        });
+        customReadFile(`./public/css${url}`, res);
+    } else if (url.indexOf(".png") !== -1) {
+        res.writeHead(httpStatus.OK, {
+            "Content-Type" : "image/png"
+        });
+        customReadFile(`./public/images${url}`, res);
+    } else {
+        sendErrorResponse(res);
+    }
+}).listen(3000);
+
+console.log(`The server is listening on port number: ${port}`);
