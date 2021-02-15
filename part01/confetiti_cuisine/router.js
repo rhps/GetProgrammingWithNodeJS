5782c7cc6bc37d1e11d97f0706d7be6ea0b55036
Rio Harapan Ps <riioharapan@gmail.com>
2021-02-16 02:14:00 +0700

    Lesson 7 Capstone - Finish all requirement capstone + logging

--- part01/confetiti_cuisine/router.js
+++ part01/confetiti_cuisine/router.js
@@ -10,9 +10,11 @@ const routes = {
 exports.handle = (req, res) => {
     try {
         routes[req.method][req.url](req, res);
+        console.log(`User Accessed: ${req.url}`);
     } catch (e) {
         res.writeHead(httpStatus.OK, contenTypes.html);
         utils.getFile("views/error.html", res);
+        console.log(`Error to acces: ${req.url}`);
     }
 };
 
