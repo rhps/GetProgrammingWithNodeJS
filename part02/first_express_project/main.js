c38a3e3396ba5fd2990965ee5a2e46b5c7c89a24
Rio Harapan Ps <riioharapan@gmail.com>
2021-02-16 17:15:43 +0700

    Lesson 8 Setting up an app with Express.js - Hello World -full

--- part02/first_express_project/main.js
+++ part02/first_express_project/main.js
@@ -2,5 +2,7 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => res.send('Hello World!'))
-.listen(port, () => console.log(`The Express.js app listening on port: ${port}!`));
+app.get('/', (req, res) => {
+    res.send('Hello World Express!');
+    console.log(req.url);
+}).listen(port, () => console.log(`The Express.js app listening on port: ${port}!`));
