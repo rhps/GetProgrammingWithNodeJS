98ab25c6b21d826872084d404f16de2b3b393cfd
Rio Harapan Ps <riioharapan@gmail.com>
2022-01-04 12:56:20 +0700

    backup

--- part02/express_templates/main.js
+++ part02/express_templates/main.js
@@ -1,11 +1,13 @@
-const express = require('express')
-const app = express()
-const homeController = require('./controllers/homeController')
+const express = require('express');
+const app = express();
+const homeController = require('./controllers/homeController');
+const layouts = require("express-ejs-layouts");
 
-app.set("port", process.env.PORT || 3000)
+app.set("port", process.env.PORT || 3000);
 app.set("view engine", "ejs");
+app.use(layouts);
 
-app.get('/', (req, res) => res.send('Hello World!'))
-app.get("/name/:myname", homeController.respondWithName)
+app.get('/', (req, res) => res.send('Hello World!'));
+app.get("/name/:myname", homeController.respondWithName);
 
 app.listen(app.get("port"), () => console.log(`Example app listening on http://localhost:${ app.get("port") }`));
